05ff09376d7ae5ebac247b57396f5949a9dc7569
Snehith Allamraju <snehith_a@yahoo.com>
2018-06-02 13:47:49 +0530

    Update ui.R
    Added text for Overview tab

--- ui.R
+++ ui.R
@@ -29,7 +29,21 @@ shinyUI(
 
       tabsetPanel(type = "tabs",
                   tabPanel("Overview",
-                           h4("Introduction")
+                           h4("Introduction"),
+                           p("This app aims to read text and using UDPipe's tokenization, tagging, lemmatization and dependency parsing functionality, it aims to provide inputs on the text file "),
+                           br(),
+                           
+                           strong("Note : This app works only for *.TXT files"),
+                           br(),
+                           br(),
+                           p("This app has three tabs "),
+                           p("a. Data : This provides the Annotated document using UDPipe's English Repo. This shows only 100 lines by default. Click the 'Download Annotated Data' button to download the full table "),
+                           p("b. WordCloud : This provides the wordclouds of most used Nouns and Verbs from your input Text "),
+                           code("Minimum frequency for occurence is defined as 2. Maximum number of words are 100"),
+                           br(),
+                           p("c. Co-Occurence plot : This provides the plot for the selected parts of speech. The input for this is the selection of Universal Parts of Speech(upos) on the left. Check for multiple selections to analyze how the co-occurence changes"),
+                           br(),
+                           code("To get started, browse & upload a text file in the file upload section on the left")
                   ),
                   tabPanel("Data",
                            h4('Annotated Document'),
@@ -39,8 +53,10 @@ shinyUI(
                            downloadButton('downloadData', "Download Annotated Data")
                   ),
                   tabPanel("WordCloud",
+
                            h4('Noun WordCloud'),                           
                            plotOutput("cloud1"),
+
                            h4('Verb WordCloud'),                           
                            plotOutput("cloud2")
                   ),
